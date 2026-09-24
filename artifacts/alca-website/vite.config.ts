d8f1b0a30741ea7f6e92daee1275d4854f5bb99e
theshubhamgundu <80262881+theshubhamgundu@users.noreply.github.com>
2026-09-24 14:58:44 +0530

    removed scarp

--- artifacts/alca-website/vite.config.ts
+++ artifacts/alca-website/vite.config.ts
@@ -3,30 +3,9 @@ import react from '@vitejs/plugin-react';
 // import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
-import runtimeErrorOverlay from '@replit/vite-plugin-runtime-error-modal';
-
-const port = Number(process.env.PORT) || 5173;
-const basePath = process.env.BASE_PATH || '/';
-
-export default defineConfig({
-  base: basePath,
   plugins: [
     react(),
     // tailwindcss(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== 'production' &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import('@replit/vite-plugin-cartographer').then((m) =>
-            m.cartographer({
-              root: path.resolve(import.meta.dirname, '..'),
-            }),
-          ),
-          await import('@replit/vite-plugin-dev-banner').then((m) =>
-            m.devBanner(),
-          ),
-        ]
-      : []),
   ],
   resolve: {
     alias: {
